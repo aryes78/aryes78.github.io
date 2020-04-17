d791bebfcf2b74498c5c332a956cee9522ee5da4
Aryes <aryes@outlook.hu>
2020-04-17 17:56:10 +0200

    Todo list

--- jq/js/script.js
+++ jq/js/script.js
@@ -1,2 +1,30 @@
-$('main').html('<h1>Hello</h1>');
-alert("Hi!");
+var counter=0;
+$('.todos-container').on('click', '.delete-button', (event) => {
+    //console.log($(event.target.parentElement));
+    $(event.target.parentElement).remove();
+});
+
+$('.todos-container').on('click', '.checked-button', (event) => {
+console.log($(event.target).attr('src'));
+$(event.target.parentElement).toggleClass('athuzott');
+if ($(event.target).attr('src') =='./res/unchecked.svg')
+    $(event.target).attr('src', './res/checked.svg');
+    else
+    $(event.target).attr('src', './res/unchecked.svg');
+});
+
+$('#addtodo').click(function (e) { 
+    $('.todos-container').append('<li><p>'+$('#todo').val()+'</p><img src="./res/delete.svg" alt="" class="delete-button"><img src="./res/unchecked.svg" alt="" class="checked-button"></li>');
+    $('#todo').val('');
+});
+
+$('#increase').click(() => { 
+    counter++;
+    $('#counter').text(counter);
+});
+
+$('#decrease').click(() => { 
+    counter--;
+    $('#counter').text(counter);
+});
+
