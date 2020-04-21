a0c8bde7ab4c7cb5cbc154f4f908b7efe378040b
Aryes <aryes@outlook.hu>
2020-04-21 22:33:59 +0200

    plus-minus buttons

--- matyas/js/script.js
+++ matyas/js/script.js
@@ -4,30 +4,60 @@ let termeles = ['norm','arany'];
 let allapot = ['egs','bet'];
 
 function openPlayer(evt, playerName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(playerName).style.display = "block";
+  evt.currentTarget.className += " active";
+
+} 
+function plus(t_id){
+  var mezo;
+  var ertek;
+  mezo = t_id.currentTarget.id.split('_');
+  ertek = $('#'+mezo[0]+'_'+mezo[1]+'_'+mezo[2]+'_'+mezo[3]).text();
+  ertek ++;
+  $('#'+mezo[0]+'_'+mezo[1]+'_'+mezo[2]+'_'+mezo[3]).text(ertek);
+  // console.log(ertek);
+  $('#'+mezo[0]+'_termeny').text(ertek);
+  $('#'+mezo[0]+'_arany').text(ertek);
+
+}
+function minus(t_id){
+  var mezo;
+  var ertek;
+  mezo = t_id.currentTarget.id.split('_');
+  ertek = $('#'+mezo[0]+'_'+mezo[1]+'_'+mezo[2]+'_'+mezo[3]).text();
+  ertek --;
+  $('#'+mezo[0]+'_'+mezo[1]+'_'+mezo[2]+'_'+mezo[3]).text(ertek);
+  $('#'+mezo[0]+'_termeny').text(ertek);
+  $('#'+mezo[0]+'_arany').text(ertek);
+}
+
+// $('#defaultOpen').click();
+$('.plus').click(function (e) { 
+  plus(e);
   
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(playerName).style.display = "block";
-    evt.currentTarget.className += " active";
-
-  } 
-
-$('#defaultOpen').click();
-
-
-jatekosok.forEach((ja) => {
-  console.log(ja);
 });
+
+$('.minus').click(function (e) { 
+  minus(e);
+  
+});
+
+// jatekosok.forEach((ja) => {
+//   console.log(ja);
+// });
